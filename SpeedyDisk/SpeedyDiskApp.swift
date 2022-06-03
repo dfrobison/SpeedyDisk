545bda290f29e1d7f6ecf19405e8c8020f567205
Doug Robison <dfrobison@gmail.com>
2022-06-02 20:45:46 -0600

    Initial Commit

---
+++ SpeedyDisk/SpeedyDiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedyDiskApp.swift
+//  SpeedyDisk
+//
+//  Created by Doug on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeedyDiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
